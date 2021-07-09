38b01ca74af39caa1fe17a8e9be3f1e0263bf9a4
raismaulana <lufriraismaulana@gmail.com>
2021-07-09 16:04:34 +0700

    base model

---
+++ application/models/Pendaftaran_keluar_model.php
@@ -0,0 +1,61 @@
+<?php
+
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Pendaftaran_keluar_model extends CI_Model {
+
+    private $table = 'pendaftaran_keluar';
+
+    public function __construct() {
+        $this->load->database();
+    }
+
+    public function insert($data)
+    {
+        $this->db->insert($this->table, $data);
+        
+    }
+
+    public function insert_last_id($data)
+    {
+        return $this->db->insert_id($this->table, $data);
+        
+    }
+
+    public function insert_batch($data)
+    {
+        $this->db->insert_batch($this->table, $data);
+    }
+
+    public function select_all()
+    {
+        return $this->db->get($this->table);
+        
+    }
+
+    public function select_where($where)
+    {
+        return $this->db->get_where($this->table, $where);
+        
+    }
+
+    public function update($where, $data)
+    {
+        $this->db->update($this->table, $data, $where);
+        
+    }
+
+    public function update_batch($key, $data)
+    {
+        $this->db->update_batch($this->table, $data, $key);
+        
+    }
+
+    public function delete($where)
+    {
+        $this->db->delete($this->table, $where);
+        
+    }
+}
+
+/* End of file Pendaftaran_keluar_model.php */
---
+++ application/models/Tingkat_prestasi_model.php
@@ -0,0 +1,61 @@
+<?php
+
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Tingkat_prestasi_model extends CI_Model {
+
+    private $table = 'tingkat_prestasi';
+
+    public function __construct() {
+        $this->load->database();
+    }
+
+    public function insert($data)
+    {
+        $this->db->insert($this->table, $data);
+        
+    }
+
+    public function insert_last_id($data)
+    {
+        return $this->db->insert_id($this->table, $data);
+        
+    }
+
+    public function insert_batch($data)
+    {
+        $this->db->insert_batch($this->table, $data);
+    }
+
+    public function select_all()
+    {
+        return $this->db->get($this->table);
+        
+    }
+
+    public function select_where($where)
+    {
+        return $this->db->get_where($this->table, $where);
+        
+    }
+
+    public function update($where, $data)
+    {
+        $this->db->update($this->table, $data, $where);
+        
+    }
+
+    public function update_batch($key, $data)
+    {
+        $this->db->update_batch($this->table, $data, $key);
+        
+    }
+
+    public function delete($where)
+    {
+        $this->db->delete($this->table, $where);
+        
+    }
+}
+
+/* End of file Tingkat_prestasi_model.php */
