88b6ce4721fe8d9dd34ca2cedbe989cf169417a8
raismaulana <lufriraismaulana@gmail.com>
2021-08-02 14:34:27 +0700

    background task

--- application/controllers/background/Cleaner.php
+++ application/controllers/background/Cleaner.php
@@ -14,7 +14,9 @@ class Cleaner extends CI_Controller
 
     public function delete_temp()
     {
-
+        if(!$this->input->is_cli_request()){
+            die('Command Line Only!');
+        }
         $path = './application/tmp/';
         delete_files($path, TRUE);
     }
